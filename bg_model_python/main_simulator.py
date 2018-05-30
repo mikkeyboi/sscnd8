1c1863554938189df83c970ba7472dcac1e71016
mikkeyboi <mleun036@uottawa.ca>
2018-05-30 02:54:56 -0400

    Main file to simulate the model

---
+++ bg_model_python/main_simulator.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+# Time Variables
+tmax=1000 # Maximum time (ms)
+dt=0.01 # Time-step (ms)
+t = np.arange(0,tmax,dt) # t=0:dt:tmax;
+n=10 # Number of neurons in each nucleus (TH, STN, GPe, GPi)
+
