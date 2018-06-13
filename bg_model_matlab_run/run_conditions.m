b45f33fe95dfbbf78f4aa0ddb9c0b7b95b7aac0d
mikkeyboi <mleun036@uottawa.ca>
2018-06-13 16:12:36 -0400

    Simulating parameters and figures

--- bg_model_matlab_run/run_conditions.m
+++ bg_model_matlab_run/run_conditions.m
@@ -1,14 +1,16 @@
 clear all
 close all
 
-addpath(genpath('D:\Studies\sscnd8\references\bg_model_matlab'))
+addpath(genpath('/home/mikkeyboi/repos/sscnd8/bg_model_matlab_run/references/bg_model_matlab'))
 %% Set initial conditions
 
 %time variables
 tmax=1000; %maximum time (ms)
 dt=0.01; %timestep (ms)
 t=0:dt:tmax;
-n=10; %number of neurons in each nucleus (TH, STN, GPe, GPi)
+n=100; %number of neurons in each nucleus (TH, STN, GPe, GPi)
+frequency=20;
+cv=0.2;
 
 %initial membrane voltages for all cells
 v1=-62+randn(n,1)*5;
@@ -18,12 +20,12 @@ v4=-62+randn(n,1)*5;
 r=randn(n,1)*2;
 
 %Sensorimotor cortex input to thalamic cells
-[Istim, timespike]=createSMC(tmax,dt,14,0.2);
+[Istim, timespike]=createSMC(tmax,dt,frequency,cv);
 % BGnetwork loads Istim.mat which has all the initial conditions
 save('Istim.mat','Istim','timespike','tmax','dt','v1','v2','v3','v4','r','n');
 
 %% Running BGnetwork.m
 h=BGnetwork(0,0,0); %healthy
-pd = BGnetwork(1,0,0); %PD
-dbs=BGnetwork(1,1,130); %PD with DBS
+%pd = BGnetwork(1,0,0); %PD
+%dbs=BGnetwork(1,1,130); %PD with DBS
 
